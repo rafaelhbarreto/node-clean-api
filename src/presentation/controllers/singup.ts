d40476f8ca529f1b93e7de835f6871194f84f46a
Rafael Barreto <rafaelhbarreto@gmail.com>
2023-02-21 15:19:14 -0300

    move duplicate code to a method helper

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,19 +1,15 @@
+import { MissingParamError } from '../errors/missing-param'
+import { badRequest } from '../helpers/http'
 import { HttpResponse, HttpRequest } from '../protocols/http'
 
 export class SingupController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: name')
-      }
+      return badRequest(new MissingParamError('name'))
     }
 
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: email')
-      }
+      return badRequest(new MissingParamError('email'))
     }
 
     return {
