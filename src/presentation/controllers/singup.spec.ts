10165a80f1f74b4f645b7ff9b9d86495963645ff
Rafael Barreto <rafaelhbarreto@gmail.com>
2023-03-02 15:04:04 -0300

    create a dependency to validate the e-mail

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -1,9 +1,32 @@
 import { MissingParamError } from '../errors/missing-param'
 import { SingupController } from './singup'
+import { EmailValidator } from '../protocols/emailValidator'
+import { InvalidParamError } from '../errors/invalid-param-error'
+
+interface stubTypes {
+  sut: SingupController
+  emailValidatorStub: EmailValidator
+}
+
+const SutFactory = (): stubTypes => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+
+  const emailValidatorStub = new EmailValidatorStub()
+  const sut = new SingupController(emailValidatorStub)
+
+  return {
+    sut,
+    emailValidatorStub
+  }
+}
 
 describe('SingupController tests', () => {
   it('should return 400 if the name is not provided', () => {
-    const sut = new SingupController()
+    const { sut } = SutFactory()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
@@ -18,7 +41,7 @@ describe('SingupController tests', () => {
   })
 
   it('should return 400 if the email is not provided', () => {
-    const sut = new SingupController()
+    const { sut } = SutFactory()
     const httpRequest = {
       body: {
         name: 'any name',
@@ -32,7 +55,7 @@ describe('SingupController tests', () => {
   })
 
   it('should return 400 if the password is not provided', () => {
-    const sut = new SingupController()
+    const { sut } = SutFactory()
     const httpRequest = {
       body: {
         name: 'any name',
@@ -46,7 +69,7 @@ describe('SingupController tests', () => {
   })
 
   it('should return 400 if the password_confirmation is not provided', () => {
-    const sut = new SingupController()
+    const { sut } = SutFactory()
     const httpRequest = {
       body: {
         name: 'any name',
@@ -57,5 +80,25 @@ describe('SingupController tests', () => {
 
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.body).toEqual(new MissingParamError('password_confirmation'))
+  })
+
+  it('should return 400 if the given e-mail is incorrect', () => {
+    const { sut, emailValidatorStub } = SutFactory()
+    const emailValidatorSpy = jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+
+    const httpRequest = {
+      body: {
+        name: 'any name',
+        email: 'any_email@mail.com',
+        password: 'any_pass',
+        password_confirmation: 'any_pass'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+    expect(emailValidatorSpy).toBeCalled()
   })
 })
--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -2,8 +2,12 @@ import { MissingParamError } from '../errors/missing-param'
 import { badRequest } from '../helpers/http'
 import { HttpResponse, HttpRequest } from '../protocols/http'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/emailValidator'
+import { InvalidParamError } from '../errors/invalid-param-error'
 
 export class SingupController implements Controller {
+  constructor (private readonly emailValidator: EmailValidator) {}
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = [
       'name',
@@ -18,9 +22,10 @@ export class SingupController implements Controller {
       }
     }
 
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: email')
+    if (!this.emailValidator.isValid(httpRequest.body.email)) {
+      return badRequest(new InvalidParamError('email'))
     }
+
+    return badRequest(new MissingParamError('email'))
   }
 }
